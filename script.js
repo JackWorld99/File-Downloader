edc55280c7e663162a186b4c44904ddd6fd6213b
JackWorld <jackwong0326@gmail.com>
2022-06-04 22:17:00 +0800

    Set the request's mode to 'no-cors' to fetch the resource with CORS disabled.

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ downloadBtn.addEventListener("click", (e) => {
 });
 
 function fetchFile(url) {
-  fetch(url)
+  fetch(url, { mode: "no-cors" })
     .then((res) => res.blob())
     .then((file) => {
       let tempUrl = URL.createObjectURL(file);
